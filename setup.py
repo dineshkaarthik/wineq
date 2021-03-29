e6b06d35138f51a0e897970224e19b42a9c4665a
dineshkaarthik <47268579+dineshkaarthik@users.noreply.github.com>
2021-03-29 21:10:58 +0530

    Setup completed

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description = "This is mlops imelementation for wineq model",
+    author = "DineshKaarthik",
+    packages = find_packages(),
+    license = "MIT"
+
+
+
+
+)
